f90db75228c452a35799ff1868c08bca5d80d924
rico0406 <ricardoroliveira12@gmail.com>
2025-11-08 20:41:24 +0000

    Add docstring players_tools.py get_player_name

--- players_tools.py
+++ players_tools.py
@@ -3,8 +3,25 @@ from typing import Dict
 
 
 def get_player_name(player_id: str) -> str:
+    """
+    Retrieve the full name of a player from Sleeper API by their player ID.
 
-    all_players: dict = Players().get_all_players()
+    Args:
+        player_id (str): The unique ID of the player.
+
+    Returns:
+        str: Full name of the player in the format 'FirstName LastName'.
+
+    Raises:
+        KeyError: If the player_id does not exist in the list of all players.
+    """
+    # Get all players as a dictionary where key is player_id and value is player info
+    all_players: Dict[str, dict] = Players().get_all_players()
+
+    # Retrieve the player information for the given player_id
     player: dict = all_players[player_id]
+
+    # Construct the full name by combining first and last name
     name: str = f"{player['first_name']} {player['last_name']}"
+
     return name
